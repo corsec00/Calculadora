e1ed2a1c09df178d53bead739482ad25c109762c
Leo Santos <corsec00@gmail.com>
2021-04-09 23:30:58 -0300

    Código corrigido

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    //Use o operador + nas variáveis para evitar a concatenação
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
